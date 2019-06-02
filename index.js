657f06b3e184f510c5a8ad080305c6a690ba23ae
Matti Luukkainen <mluukkai@iki.fi>
2019-06-02 21:34:53 +0300

    for heroku

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+const cors = require('cors')
+
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -95,7 +98,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001  
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
