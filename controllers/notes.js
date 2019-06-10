57c6fdee751082f13f129ccd399d5fbcd493caee
Matti Luukkainen <mluukkai@iki.fi>
2019-06-10 07:38:00 +0300

    part4-8

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,11 +1,20 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
     .find({}).populate('user', { username: 1, name: 1 })
-    
+
   response.json(notes.map(note => note.toJSON()))
 })
 
@@ -25,19 +34,27 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-
-  const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
-    date: new Date(),
-    user: user._id
-  })
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const note = new Note({
+      content: body.content,
+      important: body.important === undefined ? false : body.important,
+      date: new Date(),
+      user: user._id
+    })
+
     const savedNote = await note.save()
     user.notes = user.notes.concat(savedNote._id)
-    await user.save()
+    await user.save() 
     response.json(savedNote.toJSON())
   } catch (exception) {
     next(exception)
